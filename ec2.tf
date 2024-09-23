2f18cb053acdbaa897d96244149eebaab0cefe82
siva3150 <siva123@gmail.com>
2024-09-23 05:48:49 +0530

    ec2-module-demo

--- ec2.tf
+++ ec2.tf
@@ -1,3 +1,8 @@
 module "ec2" {
     source = "../terraform-aws-ec2"
+    # these are variables expected module, not arguments of resource definition
+    # You can also create variables or else you can directly use values here
+    ami_id = var.ami_something
+    instance_type = "t2.micro"
+    security_group_ids = ["sg-0e12349e784c6ec0a"]
 }
