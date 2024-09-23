ac53f22f9afc66269d422de11c7677784846fde8
siva3150 <siva123@gmail.com>
2024-09-23 06:02:22 +0530

    ec2-module-demo

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,6 @@ module "ec2" {
     # these are variables expected module, not arguments of resource definition
     # You can also create variables or else you can directly use values here
     ami_id = var.ami_something
-    instance_type = "t2.micro"
+    instance_type = "t3.large"
     security_group_ids = ["sg-0e12349e784c6ec0a"]
 }
